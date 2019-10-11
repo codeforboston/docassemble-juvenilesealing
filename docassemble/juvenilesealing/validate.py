08758cdde667d61910e8cf1d8c4ae790afdb3ce6
knod ding <knodding@gmail.com>
2019-10-11 07:44:19 -0400

    Not sure about other stuff, but zipcode work started

--- docassemble/juvenilesealing/validate.py
+++ docassemble/juvenilesealing/validate.py
@@ -1,9 +1,16 @@
 from docassemble.base.util import validation_error
 
-def is_valid_ssn(inputValue):
+def is_valid_ssn ( inputValue ):
   # I guess we'll include the dashes in the count
-  isRightLength = len(inputValue) == 11
+  isRightLength = len( inputValue ) == 11
   if ( not isRightLength ):
-    validation_error("Write the Social Security Number like this: XXX-XX-XXXX")
+    validation_error( "Write the Social Security Number like this: XXX-XX-XXXX" )
   
   return isRightLength
+
+def is_valid_zip ( inputValue ):
+    isRightLength = len( inputValue ) == 5
+    if ( not isRightLength):
+      validation_error("Write the zipcode like this: XXXXX")
+    
+    return isRightLength
