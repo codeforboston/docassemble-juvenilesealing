e8e19570a0b12b3ce4d1e657e2f0fd69308b971c
knod ding <knodding@gmail.com>
2019-10-20 09:21:19 -0400

    Mostly clarification for final instructions.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.juvenilesealing',
-      version='0.0.92',
+      version='0.0.93',
       description=('A docassemble extension for sealing juvenile records.'),
       long_description='# Sealing Juvenile Records\r\n\r\nThis process should be easier for both petitioners themselves and\r\nlegal professionals who hope to support petitioners. This tool\r\nis focused on MA.\r\n\r\n## Design\r\n\r\n### MVP should include\r\n\r\n1. Expectation setting.\r\n1. Links to support we cannot provide.\r\n1. Mobile friendly UI.\r\n1. A printable PDF.\r\n\r\n### Interview steps\r\n\r\n1. Check for immigration status **separately from the main interview**\r\n1. Screen for legal professionals\r\n1. Link to get certified dockets\r\n1. Sealing eligibility questions\r\n1. Personal details questions\r\n1. Downloadable/printable pre-filled petition PDF\r\n1. Provide the address for mailing the petition\r\n\r\n## Users\r\n\r\n### Target Audiences\r\n\r\n1. Petitioners.\r\n1. Legal professionals or clinic volunteers.\r\n\r\n### Unresovled Petitioner Barriers\r\n\r\n1. Frustration and distrust of the process.\r\n1. No access to printers.\r\n1. Which vocabulary will they find familiar?\r\n\r\n**User research needs to play a large role in the design of this tool.**\r\n\r\nThis tool is made with [Docassemble](https://github.com/jhpyle/docassemble).',
       long_description_content_type='text/markdown',
