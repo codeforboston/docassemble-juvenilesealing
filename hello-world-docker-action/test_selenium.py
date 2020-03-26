729c964dc04456f697b1f4729096fb17828066cb
berit <bjohnson413@gmail.com>
2020-03-26 19:54:06 -0400

    add python script to test selenium

---
+++ hello-world-docker-action/test_selenium.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get('www.google.com')
+screenshot = driver.save_screenshot('test.png')
+driver.quit()
