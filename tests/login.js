5059c3f11b64b2f53ac2749beaac55b566a415fa
Niharika Singh <s.niharika09@gmail.com>
2020-05-25 23:02:12 -0400

    Adding in deleteProject function

--- tests/login.js
+++ tests/login.js
@@ -18,8 +18,7 @@ const login = async () => {
   return {'page': page, 'browser': browser};
 };
 
-const createProject = async (projectName) => {
-  let {page, browser} = await login();
+const navigateToManageProject = async (page) => {
   // Go to playground
   await page.goto('https://interviews-dev.gbls.org/playground');
   // Click dropdown in top left corner
@@ -27,11 +26,16 @@ const createProject = async (projectName) => {
   await dropdown.click();
   const dropdownOptions = await page.$$('[aria-labelledby="dropdownMenuButton"] .dropdown-item');
   // Click Manage Projects button
-  const manageProjectsButton = dropdownOptions[1];
+  const manageProjectsButton = dropdownOptions[dropdownOptions.length - 1];
   await Promise.all([
     manageProjectsButton.click(),
     page.waitForNavigation(),
   ]);
+};
+
+const createProject = async (projectName) => {
+  let {page, browser} = await login();
+  await navigateToManageProject(page);
   // Click "Add a new project"
   const addNewProjectButton = await page.$('.fa-plus-circle');
   await Promise.all([
@@ -47,8 +51,27 @@ const createProject = async (projectName) => {
     saveButton.click(),
     page.waitForNavigation(),
   ]);
-  await page.screenshot({path: 'example.png'});
   await browser.close();
 };
 
-createProject('testing2');
+const deleteProject = async (projectName) => {
+  let {page, browser} = await login();
+  await navigateToManageProject(page);
+  // Click Delete button
+  const deleteLink = '[href="/playgroundproject?delete=1&project=' + projectName + '"]';
+  const deleteButton = await page.$(deleteLink);
+  await Promise.all([
+    deleteButton.click(),
+    page.waitForNavigation(),
+  ]);
+  // Click Delete button again
+  const deleteButton2 = await page.$('[type="submit"]');
+  await Promise.all([
+    deleteButton2.click(),
+    page.waitForNavigation(),
+  ]);
+  await browser.close();
+}
+
+// createProject('testing');
+// deleteProject('testing');
